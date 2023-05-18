91e79e16f2f6345c25872dde06c78efb46bb6784
Melvin Kuhlmann <melvin.kuhlmann@gmail.com>
2023-05-18 12:38:11 +0200

    Some changes on the response

--- src/main/java/nl/alvant/GreetingResource.java
+++ src/main/java/nl/alvant/GreetingResource.java
@@ -5,12 +5,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class ExampleResource {
+@Path("/greeting")
+public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello from southbound";
     }
 }
